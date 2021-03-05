3c715f1e9e4aba9c2f643f322f0211b112ecfcd5
laban405 <laban405@gmail.com>
2021-03-05 15:40:41 +0300

    trips

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -2,6 +2,8 @@ import React from "react"
 import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import { Button } from "./Button"
+import { ImLocation } from "react-icons/im"
 
 const Trips = () => {
   const data = useStaticQuery(graphql`
@@ -29,12 +31,20 @@ const Trips = () => {
     const tripsArray = []
     data.allTripsJson.edges.forEach((item, index) => {
       tripsArray.push(
-        <div key={index}>
-          <Img
+        <ProductCard key={index}>
+          <ProductImg
             src={item.node.img.childImageSharp.fluid.src}
+            alt={item.node.alt}
             fluid={item.node.img.childImageSharp.fluid}
           />
-        </div>
+          <ProductInfo>
+            <TextWrap>
+              <ImLocation />
+              <ProductTitle>{item.node.name}</ProductTitle>
+            </TextWrap>
+            <Button to="/trips">{item.node.button}</Button>
+          </ProductInfo>
+        </ProductCard>
       )
     })
     return tripsArray
@@ -61,4 +71,46 @@ const ProductsHeading = styled.div`
   margin-bottom: 5rem;
   color: #000;
 `
-const ProductWrapper = styled.div``
+const ProductWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  justify-items: center;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 868px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ProductCard = styled.div`
+  line-height: 2;
+  width: 100%;
+  height: 500px;
+  position: relative;
+  border-radius: 10px;
+  transition: 0.2s ease;
+`
+const ProductImg = styled(Img)`
+  height: 100%;
+  max-width: 100%;
+  position: relative;
+  border-radius: 10px;
+  filter: brightness(70%);
+  transition: 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+  &:hover {
+    filter: brightness(100%);
+  }
+`
+
+const ProductInfo = styled.div`
+ 
+`
+
+const TextWrap = styled.div``
+
+const ProductTitle = styled.div``
