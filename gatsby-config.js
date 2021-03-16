8a99485c9ce847ebdb841c8aaf303835ff58fb3a
laban405 <laban405@gmail.com>
2021-03-16 08:39:05 +0300

    phase one done

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Travel Site`,
+    description: `Travel the world.`,
     author: `@gatsbyjs`,
   },
   plugins: [
