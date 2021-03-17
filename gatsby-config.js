ac1b51d28f80739ff50c61b6ba6ed8657eda2383
laban405 <laban405@gmail.com>
2021-03-17 07:32:11 +0300

    deploy copy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
+  pathPrefix: `/agency-gatsby`,
   siteMetadata: {
     title: `Travel Site`,
     description: `Travel the world.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
@@ -32,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/icons/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-json`,
